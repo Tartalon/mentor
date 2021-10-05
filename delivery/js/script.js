516cb36bbf3ebdbee369b86919b2452e628cf815
Vladimir <50200804+Tartalon@users.noreply.github.com>
2021-10-05 17:51:25 +0300

    key

--- delivery/js/script.js
+++ delivery/js/script.js
@@ -7,7 +7,7 @@ fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/countries/UA/regions', {
   method: 'GET',
   headers: {
     'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com',
-    'x-rapidapi-key': '34a2e0d47fmsh2b8571b491c2fe0p1d31e2jsn7749fbdddc80',
+    'x-rapidapi-key': '',
   },
 })
   .then(response => {
