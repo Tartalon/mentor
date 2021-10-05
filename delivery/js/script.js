56180bdfee234537198aec438e372452ef03201e
Vladimir <50200804+Tartalon@users.noreply.github.com>
2021-10-05 17:46:53 +0300

    asd

--- delivery/js/script.js
+++ delivery/js/script.js
@@ -1,15 +1,18 @@
 'use ctrict';
 
-let url = 'https://jsonplaceholder.typicode.com/photos';
-let url2 = 'http://geodb-free-service.wirefreethought.com/v1/geo/cities';
-let promise = fetch(url2);
+let url =
+  'http://geodb-free-service.wirefreethought.com/v1/geo/cities?offset=0&languageCode=ru&sort=name';
 
-promise
+fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/countries/UA/regions', {
+  method: 'GET',
+  headers: {
+    'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com',
+    'x-rapidapi-key': '34a2e0d47fmsh2b8571b491c2fe0p1d31e2jsn7749fbdddc80',
+  },
+})
   .then(response => {
-    if (response.ok) {
-      alert('OK');
-    } else {
-      alert(response.status);
-    }
+    console.log(response.json());
   })
-  .catch(error => console.log(error.message));
+  .catch(err => {
+    console.error(err);
+  });
