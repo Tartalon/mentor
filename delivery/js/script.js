09ad562ad65b93e501b6e8edee98df938d31db97
Vladimir <50200804+Tartalon@users.noreply.github.com>
2021-10-06 16:11:01 +0300

    get all countries & added to the page

--- delivery/js/script.js
+++ delivery/js/script.js
@@ -1,19 +1,31 @@
 'use strict';
 
+const citiesList = document.querySelector('.cities__list');
+
 let url =
   'http://geohelper.info/api/v1/countries?apiKey=JT4TwHR95h6PpzjwXhbfwBPmffpeKSFY&locale%5Blang%5D=uk&locale%5BfallbackLang%5D=ua';
 
 fetch(url)
-  .then(response => console.log(response.json()))
-  .then(json => console.log(json))
+  .then(response => response.json())
+  .then(data => getCitiesList(mapArray(data.result)))
   .catch(err => {
     console.error(err);
   });
 
-// function mapArray(array) {
-//   return array.map(element => {
-//     return {
-//       name: element.name,
-//     };
-//   });
-// }
+function mapArray(array) {
+  return array.map(element => {
+    return {
+      name: element.name,
+    };
+  });
+}
+
+function getCitiesList(arr) {
+  arr.forEach(element => {
+    let li = document.createElement('li');
+    li.classList.add('cities__item');
+    li.textContent = element.name;
+
+    citiesList.append(li);
+  });
+}
