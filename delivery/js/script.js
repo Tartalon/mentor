d0e0b98cb81bd69d22a51e48a22ab0f37b7c7b06
Tartalon <tartalon23@gmail.com>
2021-10-10 16:59:40 +0300

    phone mask

--- delivery/js/script.js
+++ delivery/js/script.js
@@ -1,32 +1,29 @@
-'use strict';
+"use strict";
 
-const citiesList = document.querySelector('.cities__list');
-const citiesInput = document.querySelector('.cities__input');
-const dropdownButton = document.querySelector('.dropdown__button');
-const citiesListWrapper = document.querySelector('.cities__list-wrapper');
+const citiesList = document.querySelector(".cities__list");
+const citiesInput = document.querySelector(".cities__input");
+const dropdownButton = document.querySelector(".dropdown__button");
+const citiesListWrapper = document.querySelector(".cities__list-wrapper");
 
 let url =
-  'http://geohelper.info/api/v1/countries?apiKey=JT4TwHR95h6PpzjwXhbfwBPmffpeKSFY&locale%5Blang%5D=uk&locale%5BfallbackLang%5D=ua';
+  "http://geohelper.info/api/v1/countries?apiKey=JT4TwHR95h6PpzjwXhbfwBPmffpeKSFY&locale%5Blang%5D=uk&locale%5BfallbackLang%5D=ua";
 
 fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    getCitiesList(mapArray(data.result));
-    citiesInput.addEventListener(
-      'change',
-      arrayFilterByInput(mapArray(data.result))
-    );
+  .then((response) => response.json())
+  .then((data) => {
+    let citiesArray = mapArray(data.result);
+    getCitiesList(citiesArray);
   })
-  .catch(err => {
+  .catch((err) => {
     console.error(err);
   });
 
-dropdownButton.addEventListener('click', function () {
-  citiesListWrapper.classList.toggle('hidden');
+dropdownButton.addEventListener("click", function () {
+  citiesListWrapper.classList.toggle("hidden");
 });
 
 function mapArray(array) {
-  return array.map(element => {
+  return array.map((element) => {
     return {
       name: element.name,
     };
@@ -34,17 +31,17 @@ function mapArray(array) {
 }
 
 function getCitiesList(arr) {
-  arr.forEach(element => {
-    let li = document.createElement('li');
-    li.classList.add('cities__item');
+  arr.forEach((element) => {
+    let li = document.createElement("li");
+    li.classList.add("cities__item");
     li.textContent = element.name;
 
     citiesList.append(li);
   });
 }
 
-function arrayFilterByInput(array) {
-  let value = citiesInput.value;
-  let result = array.filter(city => value + city.slice(value.length));
-  return result;
-}
+var phone = document.getElementById("PhoneInput");
+var maskOptions = {
+  mask: "+{38}(000)000-00-00",
+};
+var mask = IMask(phone, maskOptions);
