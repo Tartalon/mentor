af6510687e5c8476cf75048cafd0f0a143ea1ced
Tartalon <tartalon23@gmail.com>
2021-10-10 18:59:09 +0300

    inputs for hight & width

--- delivery/js/script.js
+++ delivery/js/script.js
@@ -4,6 +4,10 @@ const citiesList = document.querySelector(".cities__list");
 const citiesInput = document.querySelector(".cities__input");
 const dropdownButton = document.querySelector(".dropdown__button");
 const citiesListWrapper = document.querySelector(".cities__list-wrapper");
+const cargoWidthInp = document.querySelector("#CargoWidth");
+const cargoHightInp = document.querySelector("#CargoHight");
+const cargoHightRange = document.querySelector(".hight__range");
+const cargoWidthRange = document.querySelector(".width__range");
 
 let url =
   "http://geohelper.info/api/v1/countries?apiKey=JT4TwHR95h6PpzjwXhbfwBPmffpeKSFY&locale%5Blang%5D=uk&locale%5BfallbackLang%5D=ua";
@@ -40,8 +44,12 @@ function getCitiesList(arr) {
   });
 }
 
-var phone = document.getElementById("PhoneInput");
+cargoHightRange.addEventListener("change", function (e) {
+  console.log(cargoHightRange);
+});
+
+var phoneMask = document.getElementById("PhoneInput");
 var maskOptions = {
   mask: "+{38}(000)000-00-00",
 };
-var mask = IMask(phone, maskOptions);
+var mask = IMask(phoneMask, maskOptions);
