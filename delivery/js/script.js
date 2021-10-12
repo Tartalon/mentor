1c7f7f9d1dd13447b9f0bd7da14995dd4a0f96f6
Tartalon <tartalon23@gmail.com>
2021-10-12 22:55:53 +0300

    weight output in kg. Bug - one step behind

--- delivery/js/script.js
+++ delivery/js/script.js
@@ -1,52 +1,60 @@
-'use strict';
+"use strict";
 
-const citiesList = document.querySelector('.cities__list');
-const citiesInput = document.querySelector('.cities__input');
-const dropdownButton = document.querySelector('.dropdown__button');
-const citiesListWrapper = document.querySelector('.cities__list-wrapper');
-const cargoInputs = document.querySelector('.cargo__inputs');
-const cargoWidthInp = document.querySelector('#CargoWidth');
-const cargoHightInp = document.querySelector('#CargoHight');
-const cargoHightRange = document.querySelector('.hight__range');
-const cargoWidthRange = document.querySelector('.width__range');
-const cargoDrawing = document.querySelector('.cargo__drawing');
+const citiesList = document.querySelector(".cities__list");
+const citiesInput = document.querySelector(".cities__input");
+const dropdownButton = document.querySelector(".dropdown__button");
+const citiesListWrapper = document.querySelector(".cities__list-wrapper");
+const cargoInputs = document.querySelector(".cargo__inputs");
+const cargoWidthInp = document.querySelector("#CargoWidth");
+const cargoHightInp = document.querySelector("#CargoHight");
+const cargoHightRange = document.querySelector(".hight__range");
+const cargoWidthRange = document.querySelector(".width__range");
+const cargoDrawing = document.querySelector(".cargo__drawing");
+const cargoWeight = document.querySelector(".cargo__weight");
 
 let url =
-  'http://geohelper.info/api/v1/countries?apiKey=JT4TwHR95h6PpzjwXhbfwBPmffpeKSFY&locale%5Blang%5D=uk&locale%5BfallbackLang%5D=ua';
+  "http://geohelper.info/api/v1/countries?apiKey=JT4TwHR95h6PpzjwXhbfwBPmffpeKSFY&locale%5Blang%5D=uk&locale%5BfallbackLang%5D=ua";
 
 fetch(url)
-  .then(response => response.json())
-  .then(data => {
+  .then((response) => response.json())
+  .then((data) => {
     let citiesArray = mapArray(data.result);
     getCitiesList(citiesArray);
   })
-  .catch(err => {
+  .catch((err) => {
     console.error(err);
   });
 
-dropdownButton.addEventListener('click', function () {
-  citiesListWrapper.classList.toggle('hidden');
+dropdownButton.addEventListener("click", function () {
+  citiesListWrapper.classList.toggle("hidden");
 });
 
-cargoInputs.addEventListener('input', function (e) {
+cargoInputs.addEventListener("input", function (e) {
   let target = e.target;
-  let height = +cargoHightInp.value * 100 + 20;
-  let width = +cargoWidthInp.value * 100 + 20;
+  let inputWidth = cargoWidthInp.value;
+  let rangeWidth = cargoWidthRange.value;
+  let inputHight = cargoHightInp.value;
+  let rangeHight = cargoHightRange.value;
+  let height = inputHight * 100 + 20;
+  let width = inputWidth * 100 + 20;
 
   if (target === cargoWidthRange || target === cargoHightRange) {
-    cargoWidthInp.value = cargoWidthRange.value;
-    cargoHightInp.value = cargoHightRange.value;
+    cargoWidthInp.value = rangeWidth;
+    cargoHightInp.value = rangeHight;
   } else if (target === cargoWidthInp || target === cargoHightInp) {
-    cargoWidthRange.value = cargoWidthInp.value;
-    cargoHightRange.value = cargoHightInp.value;
+    rangeWidth = inputWidth;
+    rangeHight = inputHight;
   }
 
   cargoDrawing.style.minWidth = `${width}px`;
-  cargoDrawing.style.minHeight = height + 'px';
+  cargoDrawing.style.minHeight = height + "px";
+
+  cargoWeight.textContent =
+    Math.round((inputHight * inputWidth) / 0.0017) + " Kg";
 });
 
 function mapArray(array) {
-  return array.map(element => {
+  return array.map((element) => {
     return {
       name: element.name,
     };
@@ -54,17 +62,17 @@ function mapArray(array) {
 }
 
 function getCitiesList(arr) {
-  arr.forEach(element => {
-    let li = document.createElement('li');
-    li.classList.add('cities__item');
+  arr.forEach((element) => {
+    let li = document.createElement("li");
+    li.classList.add("cities__item");
     li.textContent = element.name;
 
     citiesList.append(li);
   });
 }
 
-var phoneMask = document.getElementById('PhoneInput');
+var phoneMask = document.getElementById("PhoneInput");
 var maskOptions = {
-  mask: '+{38}(000)000-00-00',
+  mask: "+{38}(000)000-00-00",
 };
 var mask = IMask(phoneMask, maskOptions);
