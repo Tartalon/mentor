cbb17b3bca49b8ffccf3612cea6069b10055dd70
Tartalon <tartalon23@gmail.com>
2021-10-14 18:22:02 +0300

    remove function mapArray

--- delivery/js/script.js
+++ delivery/js/script.js
@@ -1,35 +1,39 @@
-'use strict';
+"use strict";
 
-const citiesList = document.querySelector('.cities__list');
-const citiesInput = document.querySelector('.cities__input');
-const dropdownButton = document.querySelector('.dropdown__button');
-const citiesListWrapper = document.querySelector('.cities__list-wrapper');
-const cargoInputs = document.querySelector('.cargo__inputs');
-const cargoWidthInp = document.querySelector('#CargoWidth');
-const cargoHightInp = document.querySelector('#CargoHight');
-const cargoHightRange = document.querySelector('.hight__range');
-const cargoWidthRange = document.querySelector('.width__range');
-const cargoDrawing = document.querySelector('.cargo__drawing');
-const cargoWeight = document.querySelector('.cargo__weight');
+const citiesList = document.querySelector(".cities__list");
+const citiesInput = document.querySelector(".cities__input");
+const dropdownButton = document.querySelector(".dropdown__button");
+const citiesListWrapper = document.querySelector(".cities__list-wrapper");
+const cargoInputs = document.querySelector(".cargo__inputs");
+const cargoWidthInp = document.querySelector("#CargoWidth");
+const cargoHightInp = document.querySelector("#CargoHight");
+const cargoHightRange = document.querySelector(".hight__range");
+const cargoWidthRange = document.querySelector(".width__range");
+const cargoDrawing = document.querySelector(".cargo__drawing");
+const cargoWeight = document.querySelector(".cargo__weight");
 
 let url =
-  'http://geohelper.info/api/v1/countries?apiKey=JT4TwHR95h6PpzjwXhbfwBPmffpeKSFY&locale%5Blang%5D=uk&locale%5BfallbackLang%5D=ua';
+  "http://geohelper.info/api/v1/countries?apiKey=JT4TwHR95h6PpzjwXhbfwBPmffpeKSFY&locale%5Blang%5D=uk&locale%5BfallbackLang%5D=ua";
 
 fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    let citiesArray = mapArray(data.result);
+  .then((response) => response.json())
+  .then((data) => {
+    let citiesArray = data.result.map((element) => {
+      return {
+        name: element.name,
+      };
+    });
     getCitiesList(citiesArray);
   })
-  .catch(err => {
+  .catch((err) => {
     console.error(err);
   });
 
-dropdownButton.addEventListener('click', function () {
-  citiesListWrapper.classList.toggle('hidden');
+dropdownButton.addEventListener("click", function () {
+  citiesListWrapper.classList.toggle("hidden");
 });
 
-cargoInputs.addEventListener('input', function (e) {
+cargoInputs.addEventListener("input", function (e) {
   let target = e.target;
   let inputWidth = cargoWidthInp.value;
   let rangeWidth = cargoWidthRange.value;
@@ -39,7 +43,7 @@ cargoInputs.addEventListener('input', function (e) {
   let width = inputWidth * 100 + 20;
 
   cargoWeight.textContent =
-    Math.round((rangeHight * rangeWidth) / 0.0017) + ' Kg';
+    Math.round((rangeHight * rangeWidth) / 0.0017) + " Kg";
 
   cargoDrawing.style.minWidth = `${width}px`;
   cargoDrawing.style.minHeight = `${height}px`;
@@ -53,31 +57,23 @@ cargoInputs.addEventListener('input', function (e) {
   }
 });
 
-function mapArray(array) {
-  return array.map(element => {
-    return {
-      name: element.name,
-    };
-  });
-}
-
 function getCitiesList(arr) {
-  arr.forEach(element => {
-    let li = document.createElement('li');
-    li.classList.add('cities__item');
+  arr.forEach((element) => {
+    let li = document.createElement("li");
+    li.classList.add("cities__item");
     li.textContent = element.name;
 
     citiesList.append(li);
   });
 }
 
-const phoneMask = document.getElementById('PhoneInput');
+const phoneMask = document.getElementById("PhoneInput");
 const maskOptions = {
-  mask: '+{38}(000)000-00-00',
+  mask: "+{38}(000)000-00-00",
 };
 const mask = IMask(phoneMask, maskOptions);
 
 (function () {
   let date = new Date();
-  document.querySelector('#dateInput').valueAsDate = new Date();
+  document.querySelector("#dateInput").valueAsDate = new Date();
 })();
