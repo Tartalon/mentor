6b82c32d3638a866274c21ed4395827f42886c4b
Tartalon <tartalon23@gmail.com>
2021-10-17 20:53:33 +0300

    added filterCities on input

--- delivery/js/script.js
+++ delivery/js/script.js
@@ -13,16 +13,18 @@ const cargoDrawing = document.querySelector(".cargo__drawing");
 const cargoWeight = document.querySelector(".cargo__weight");
 
 const out = document.querySelector(".out");
+let citiesArr = [];
 
 const url =
   "https://gist.githubusercontent.com/alex-oleshkevich/1509c308fabab9e104b5190dab99a77b/raw/b20bd8026deec00205a57d395c0ae1f75cc387bb/ua-cities.json";
 
 (async function getData() {
+  clearInput(citiesInput);
   const response = await fetch(url);
   const data = await response.json();
   const regions = await data[0].regions;
   const regionsCitiesArray = regions.map((element) => element.cities);
-  let citiesArr = [];
+
   regionsCitiesArray.map((element) => {
     for (let i = 0; i < element.length; i++) {
       citiesArr.push(element[i].name);
@@ -31,35 +33,22 @@ const url =
   createLi(citiesArr);
 })();
 
-function createLi(arr) {
-  for (const city of arr) {
-    const li = document.createElement("li");
-    li.classList.add("cities__item");
-    li.textContent = city;
-    citiesList.append(li);
-  }
-}
-
-// const url =
-//   "http://geohelper.info/api/v1/countries?apiKey=JT4TwHR95h6PpzjwXhbfwBPmffpeKSFY&locale%5Blang%5D=uk&locale%5BfallbackLang%5D=ua";
-
-// fetch(url)
-//   .then((response) => response.json())
-//   .then((data) => {
-//     let citiesArray = data.result.map((element) => {
-//       return {
-//         name: element.name,
-//       };
-//     });
-//     getCitiesList(citiesArray);
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
-
 dropdownButton.addEventListener("click", function () {
   citiesListWrapper.classList.toggle("hidden");
 });
+
+citiesInput.addEventListener("input", function () {
+  citiesListWrapper.classList.remove("hidden");
+  let inputValue = citiesInput.value;
+  let filteredCities = citiesArr.filter(
+    (city) =>
+      city.slice(0, inputValue.length) + city.slice(inputValue.length) ===
+      inputValue + city.slice(inputValue.length)
+  );
+  createLi(filteredCities);
+});
+
+citiesList.addEventListener("click", choossesCity);
 
 cargoInputs.addEventListener("input", function (e) {
   let target = e.target;
@@ -85,6 +74,20 @@ cargoInputs.addEventListener("input", function (e) {
   }
 });
 
+function createLi(arr) {
+  let sortedArr = arr.sort();
+  if (citiesList.children) {
+    let liArray = Array.from(citiesList.children);
+    liArray.forEach((element) => element.remove());
+  }
+  for (const city of sortedArr) {
+    const li = document.createElement("li");
+    li.classList.add("cities__item");
+    li.textContent = city;
+    citiesList.append(li);
+  }
+}
+
 function getCitiesList(arr) {
   arr.forEach((element) => {
     let li = document.createElement("li");
@@ -95,13 +98,32 @@ function getCitiesList(arr) {
   });
 }
 
+function choossesCity(event) {
+  let target = event.target;
+  citiesInput.value = target.textContent;
+  citiesListWrapper.classList.add("hidden");
+}
+
+(function () {
+  document.querySelector("#dateInput").valueAsDate = new Date();
+})();
+
+function clearInput(input) {
+  input.textContent = "";
+  input.value = "";
+}
+
+function filterCities(arr) {
+  let inputValue = citiesInput.value;
+  let filteredCities = arr.filter(
+    (city) => city.slice(0, inputValue.length) == inputValue
+  );
+  createLi(filteredCities);
+  console.log(filteredCities);
+}
+
 const phoneMask = document.getElementById("PhoneInput");
 const maskOptions = {
   mask: "+{38}(000)000-00-00",
 };
 const mask = IMask(phoneMask, maskOptions);
-
-(function () {
-  let date = new Date();
-  document.querySelector("#dateInput").valueAsDate = new Date();
-})();
