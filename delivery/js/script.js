e07ffb2cd1c8aa4d2c497fb25a41fce59b088629
Vladimir <50200804+Tartalon@users.noreply.github.com>
2021-10-20 17:50:40 +0300

    try function getCostOfDelivery

--- delivery/js/script.js
+++ delivery/js/script.js
@@ -1,24 +1,24 @@
-"use strict";
+'use strict';
 
-const citiesList = document.querySelector(".cities__list");
-const citiesInput = document.querySelector(".cities__input");
-const dropdownButton = document.querySelector(".dropdown__button");
-const citiesListWrapper = document.querySelector(".cities__list-wrapper");
-const cargoInputs = document.querySelector(".cargo__inputs");
-const cargoWidthInp = document.querySelector("#CargoWidth");
-const cargoHightInp = document.querySelector("#CargoHight");
-const cargoHightRange = document.querySelector(".hight__range");
-const cargoWidthRange = document.querySelector(".width__range");
-const cargoDrawing = document.querySelector(".cargo__drawing");
-const cargoWeight = document.querySelector(".cargo__weight");
-const dateInput = document.querySelector("#dateInput");
-const timeInput = document.querySelector("#timeInput");
+const citiesList = document.querySelector('.cities__list');
+const citiesInput = document.querySelector('.cities__input');
+const dropdownButton = document.querySelector('.dropdown__button');
+const citiesListWrapper = document.querySelector('.cities__list-wrapper');
+const cargoInputs = document.querySelector('.cargo__inputs');
+const cargoWidthInp = document.querySelector('#CargoWidth');
+const cargoHightInp = document.querySelector('#CargoHight');
+const cargoHightRange = document.querySelector('.hight__range');
+const cargoWidthRange = document.querySelector('.width__range');
+const cargoDrawing = document.querySelector('.cargo__drawing');
+const cargoWeight = document.querySelector('.cargo__weight');
+const dateInput = document.querySelector('#dateInput');
+const timeInput = document.querySelector('#timeInput');
+const finalCost = document.querySelector('.finalCost');
 
-const out = document.querySelector(".out");
 let citiesArr = [];
 
 const url =
-  "https://gist.githubusercontent.com/alex-oleshkevich/1509c308fabab9e104b5190dab99a77b/raw/b20bd8026deec00205a57d395c0ae1f75cc387bb/ua-cities.json";
+  'https://gist.githubusercontent.com/alex-oleshkevich/1509c308fabab9e104b5190dab99a77b/raw/b20bd8026deec00205a57d395c0ae1f75cc387bb/ua-cities.json';
 
 (async function getData() {
   try {
@@ -26,9 +26,9 @@ const url =
     const response = await fetch(url);
     const data = await response.json();
     const regions = data[0].regions;
-    regions.forEach((reg) => {
+    regions.forEach(reg => {
       const cities = [...reg.cities];
-      cities.forEach((city) => {
+      cities.forEach(city => {
         citiesArr.push(city.name);
       });
     });
@@ -38,24 +38,24 @@ const url =
   }
 })();
 
-dropdownButton.addEventListener("click", function () {
-  citiesListWrapper.classList.toggle("hidden");
+dropdownButton.addEventListener('click', function () {
+  citiesListWrapper.classList.toggle('hidden');
 });
 
-dateInput.setAttribute("min", getDeliveryDate());
+dateInput.setAttribute('min', getDeliveryDate());
 
-citiesInput.addEventListener("input", function (e) {
-  citiesListWrapper.classList.remove("hidden");
+citiesInput.addEventListener('input', function (e) {
+  citiesListWrapper.classList.remove('hidden');
   const inputValue = citiesInput.value;
   let filteredCities = citiesArr.filter(
-    (city) => city.toLowerCase().indexOf(e.target.value.toLowerCase()) > -1
+    city => city.toLowerCase().indexOf(e.target.value.toLowerCase()) > -1
   );
   createLi(filteredCities);
 });
 
-citiesList.addEventListener("click", choossesCity);
+citiesList.addEventListener('click', choossesCity);
 
-cargoInputs.addEventListener("input", function (e) {
+cargoInputs.addEventListener('input', function (e) {
   let target = e.target;
   let inputWidth = cargoWidthInp.value;
   let rangeWidth = cargoWidthRange.value;
@@ -65,10 +65,7 @@ cargoInputs.addEventListener("input", function (e) {
   let width = inputWidth * 100 + 20;
 
   cargoWeight.textContent =
-    Math.round((rangeHight * rangeWidth) / 0.0017) + " Kg";
-
-  cargoDrawing.style.minWidth = `${width}px`;
-  cargoDrawing.style.minHeight = `${height}px`;
+    Math.round((rangeHight * rangeWidth) / 0.02) + ' Kg';
 
   if (target === cargoWidthRange || target === cargoHightRange) {
     cargoWidthInp.value = rangeWidth;
@@ -77,26 +74,29 @@ cargoInputs.addEventListener("input", function (e) {
     cargoWidthRange.value = inputWidth;
     cargoHightRange.value = inputHight;
   }
+
+  cargoDrawing.style.minWidth = `${width}px`;
+  cargoDrawing.style.minHeight = `${height}px`;
 });
 
 function createLi(arr) {
   let sortedArr = arr.sort();
   if (citiesList.children) {
     let liArray = Array.from(citiesList.children);
-    liArray.forEach((element) => element.remove());
+    liArray.forEach(element => element.remove());
   }
   for (const city of sortedArr) {
-    const li = document.createElement("li");
-    li.classList.add("cities__item");
+    const li = document.createElement('li');
+    li.classList.add('cities__item');
     li.textContent = city;
     citiesList.append(li);
   }
 }
 
 function getCitiesList(arr) {
-  arr.forEach((element) => {
-    let li = document.createElement("li");
-    li.classList.add("cities__item");
+  arr.forEach(element => {
+    let li = document.createElement('li');
+    li.classList.add('cities__item');
     li.textContent = element.name;
 
     citiesList.append(li);
@@ -106,22 +106,18 @@ function getCitiesList(arr) {
 function choossesCity(event) {
   let target = event.target;
   citiesInput.value = target.textContent;
-  citiesListWrapper.classList.add("hidden");
+  citiesListWrapper.classList.add('hidden');
 }
 
-(function () {
-  document.querySelector("#dateInput").valueAsDate = new Date();
-})();
-
 function clearInput(input) {
-  input.textContent = "";
-  input.value = "";
+  input.textContent = '';
+  input.value = '';
 }
 
 function filterCities(arr) {
   let inputValue = citiesInput.value;
   let filteredCities = arr.filter(
-    (city) => city.slice(0, inputValue.length) == inputValue
+    city => city.slice(0, inputValue.length) == inputValue
   );
   createLi(filteredCities);
   console.log(filteredCities);
@@ -137,23 +133,16 @@ function getDeliveryDate() {
   return deliweryDay;
 }
 
-const phoneMask = document.getElementById("PhoneInput");
+function getCostOfDelivery(width, hight, weight) {
+  const cost = 0;
+  const costForWidt = width > 1 ? (width - 1) * 1000 : 0;
+  const costForHight = hight > 1 ? (hight - 1) * 1200 : 0;
+}
+
+const phoneMask = document.getElementById('PhoneInput');
 const maskOptions = {
-  mask: "+{38}(000)000-00-00",
+  mask: '+{38}(000)000-00-00',
 };
 const mask = IMask(phoneMask, maskOptions);
 
-// (async function getData() {
-//   clearInput(citiesInput);
-//   const response = await fetch(url);
-//   const data = await response.json();
-
-//   const regions = data[0].regions;
-//   regions.forEach((r) => {
-//     const cities = [...r.cities];
-//     cities.forEach((c) => {
-//       citiesArr.push(c.name);
-//     });
-//   });
-//   createLi(citiesArr);
-// })();
+// Каждый дополнительный метр ширины груза: +1000 рублей к стоимости доставки - Каждый дополнительный метр высоты груза: +1200 рублей к стоимости доставки - Если вес больше половинного значения максимально допустимого (например, вы указали максимальный вес 500кг, а пользователь ввел >250): +3500 к стоимости доставки
