bf804cbd1bfdcbd740bbce3bc35cc1403e4a003a
Vladimir <50200804+Tartalon@users.noreply.github.com>
2021-11-11 18:44:37 +0200

    123

--- delivery/js/script.js
+++ delivery/js/script.js
@@ -1,21 +1,23 @@
-"use strict";
+'use strict';
 
-const citiesList = document.querySelector(".cities__list");
-const citiesInput = document.querySelector(".cities__input");
-const dropdownButton = document.querySelector(".dropdown__button");
-const citiesListWrapper = document.querySelector(".cities__list-wrapper");
-const cargoInputs = document.querySelector(".cargo__inputs");
-const cargoWidthInp = document.querySelector("#CargoWidth");
-const cargoHightInp = document.querySelector("#CargoHight");
-const cargoHightRange = document.querySelector(".hight__range");
-const cargoWidthRange = document.querySelector(".width__range");
-const cargoDrawing = document.querySelector(".cargo__drawing");
-const cargoWeight = document.querySelector(".cargo__weight");
-const dateInput = document.querySelector("#dateInput");
-const timeInput = document.querySelector("#timeInput");
-const finalCost = document.querySelector(".finalCost span");
-const confirmButton = document.querySelector(".confirm-button");
-const phoneMask = document.getElementById("PhoneInput");
+const form = document.querySelector('form');
+const citiesList = document.querySelector('.cities__list');
+const citiesInput = document.querySelector('.cities__input');
+const dropdownButton = document.querySelector('.dropdown__button');
+const citiesListWrapper = document.querySelector('.cities__list-wrapper');
+const cargoInputs = document.querySelector('.cargo__inputs');
+const cargoWidthInp = document.querySelector('#CargoWidth');
+const cargoHightInp = document.querySelector('#CargoHight');
+const cargoHightRange = document.querySelector('.hight__range');
+const cargoWidthRange = document.querySelector('.width__range');
+const cargoDrawing = document.querySelector('.cargo__drawing');
+const cargoWeight = document.querySelector('.cargo__weight');
+const dateInput = document.querySelector('#dateInput');
+const timeInput = document.querySelector('#timeInput');
+const finalCost = document.querySelector('.finalCost span');
+const confirmButton = document.querySelector('.confirm-button');
+const phoneMask = document.getElementById('PhoneInput');
+const modal = document.querySelector('.modal');
 
 const prices = {
   transportationPrice: 35,
@@ -30,7 +32,7 @@ let answers = {};
 let citiesArr = [];
 
 const url =
-  "https://gist.githubusercontent.com/alex-oleshkevich/1509c308fabab9e104b5190dab99a77b/raw/b20bd8026deec00205a57d395c0ae1f75cc387bb/ua-cities.json";
+  'https://gist.githubusercontent.com/alex-oleshkevich/1509c308fabab9e104b5190dab99a77b/raw/b20bd8026deec00205a57d395c0ae1f75cc387bb/ua-cities.json';
 
 (async function getData() {
   try {
@@ -38,9 +40,9 @@ const url =
     const response = await fetch(url);
     const data = await response.json();
     const regions = data[0].regions;
-    regions.forEach((reg) => {
+    regions.forEach(reg => {
       const cities = [...reg.cities];
-      cities.forEach((city) => {
+      cities.forEach(city => {
         citiesArr.push(city.name);
       });
     });
@@ -50,24 +52,30 @@ const url =
   }
 })();
 
-dropdownButton.addEventListener("click", function () {
-  citiesListWrapper.classList.toggle("hidden");
+dropdownButton.addEventListener('click', function () {
+  citiesListWrapper.classList.toggle('hidden');
 });
 
-dateInput.setAttribute("min", getDeliveryDate());
+form.addEventListener('click', function (e) {
+  if (e.target != citiesList && e.target != dropdownButton) {
+    citiesListWrapper.classList.add('hidden');
+  }
+});
 
-citiesInput.addEventListener("input", function (e) {
-  citiesListWrapper.classList.remove("hidden");
+dateInput.setAttribute('min', getDeliveryDate());
+
+citiesInput.addEventListener('input', function (e) {
+  citiesListWrapper.classList.remove('hidden');
 
   let filteredCities = citiesArr.filter(
-    (city) => city.toLowerCase().indexOf(e.target.value.toLowerCase()) > -1
+    city => city.toLowerCase().indexOf(e.target.value.toLowerCase()) > -1
   );
   createLi(filteredCities);
 });
 
-citiesList.addEventListener("click", choossesCity);
+citiesList.addEventListener('click', choossesCity);
 
-cargoInputs.addEventListener("input", function (e) {
+cargoInputs.addEventListener('input', function (e) {
   const target = e.target;
 
   let priceWithoutExtraCharge = 0;
@@ -84,7 +92,7 @@ cargoInputs.addEventListener("input", function (e) {
   const weight = Math.round(
     (cargoHightRange.value * cargoWidthRange.value) / 0.02
   );
-  cargoWeight.textContent = weight + " Kg";
+  cargoWeight.textContent = weight + ' Kg';
 
   const height = cargoHightInp.value * 100 + 20;
   const width = cargoWidthInp.value * 100 + 20;
@@ -98,36 +106,44 @@ cargoInputs.addEventListener("input", function (e) {
     cargoHightInp.value,
     weight
   );
-  finalCost.textContent = priceWithoutExtraCharge + totalPrice + " UAH";
+  finalCost.textContent = priceWithoutExtraCharge + totalPrice + ' UAH';
 });
 
-confirmButton.addEventListener("click", writeDownTheAnswers);
-confirmButton.addEventListener("click", createModal);
-confirmButton.addEventListener("click", citiesInputValidation);
-confirmButton.addEventListener("click", nameInputValidation);
-confirmButton.addEventListener("click", surnameInputValidation);
-confirmButton.addEventListener("click", phoneInputValidation);
-confirmButton.addEventListener("click", dateInputValidation);
-confirmButton.addEventListener("click", timeInputValidation);
+confirmButton.addEventListener('click', writeDownTheAnswers);
+confirmButton.addEventListener('click', function () {
+  const values = Object.values(answers);
+  if (values.includes('')) {
+    return;
+  } else {
+    createModal();
+    modal.style.visibility = 'visible';
+  }
+});
+confirmButton.addEventListener('click', citiesInputValidation);
+confirmButton.addEventListener('click', nameInputValidation);
+confirmButton.addEventListener('click', surnameInputValidation);
+confirmButton.addEventListener('click', phoneInputValidation);
+confirmButton.addEventListener('click', dateInputValidation);
+confirmButton.addEventListener('click', timeInputValidation);
 
 function createLi(arr) {
   let sortedArr = arr.sort();
   if (citiesList.children) {
     let liArray = Array.from(citiesList.children);
-    liArray.forEach((element) => element.remove());
+    liArray.forEach(element => element.remove());
   }
   for (const city of sortedArr) {
-    const li = document.createElement("li");
-    li.classList.add("cities__item");
+    const li = document.createElement('li');
+    li.classList.add('cities__item');
     li.textContent = city;
     citiesList.append(li);
   }
 }
 
 function getCitiesList(arr) {
-  arr.forEach((element) => {
-    let li = document.createElement("li");
-    li.classList.add("cities__item");
+  arr.forEach(element => {
+    let li = document.createElement('li');
+    li.classList.add('cities__item');
     li.textContent = element.name;
 
     citiesList.append(li);
@@ -137,18 +153,18 @@ function getCitiesList(arr) {
 function choossesCity(event) {
   let target = event.target;
   citiesInput.value = target.textContent;
-  citiesListWrapper.classList.add("hidden");
+  citiesListWrapper.classList.add('hidden');
 }
 
 function clearInput(input) {
-  input.textContent = "";
-  input.value = "";
+  input.textContent = '';
+  input.value = '';
 }
 
 function filterCities(arr) {
   let inputValue = citiesInput.value;
   let filteredCities = arr.filter(
-    (city) => city.slice(0, inputValue.length) == inputValue
+    city => city.slice(0, inputValue.length) == inputValue
   );
   createLi(filteredCities);
 }
@@ -178,10 +194,10 @@ function getAdditionalPrice(width, height, weight) {
 }
 
 function writeDownTheAnswers() {
-  const nameInput = document.querySelector("#NameInput");
-  const surnameInput = document.querySelector("#SurnameInput");
-  const phoneInput = document.querySelector("#PhoneInput");
-  const commentsInput = document.querySelector(".comment__input");
+  const nameInput = document.querySelector('#NameInput');
+  const surnameInput = document.querySelector('#SurnameInput');
+  const phoneInput = document.querySelector('#PhoneInput');
+  const commentsInput = document.querySelector('.comment__input');
 
   answers.city = citiesInput.value;
   answers.name = nameInput.value;
@@ -195,20 +211,20 @@ function writeDownTheAnswers() {
 }
 
 function createModal() {
-  const modal = document.querySelector(".modal__box");
-  modal.innerHTML = "";
+  const modal = document.querySelector('.modal__box');
+  modal.innerHTML = '';
 
   modal.insertAdjacentHTML(
-    "afterbegin",
+    'afterbegin',
     `
     <p class="recipient__city">Recipient city: ${answers.city}</p>
     <p class="recipient__name">Full name: ${answers.name} ${answers.surname}</p>
     <p class="recipient__phone">Recipient phone number: ${answers.phone}</p>
     <p class="comments">Comments: </p>
     <div class="cargo__info-box">
-      <span class="cargo__width">Cargo width: ${answers.width}m</span>
-      <span class="cargo__height">Cargo height: ${answers.hight}m</span>
-      <span class="cargo__weight">Cargo weight: ${answers.weight}kg</span>
+      <span class="cargo__width cargo__span">Cargo width: ${answers.width}m</span>
+      <span class="cargo__height cargo__span">Cargo height: ${answers.hight}m</span>
+      <span class="cargo__weight cargo__span">Cargo weight: ${answers.weight}kg</span>
     </div>
     <p class="delivery-date">Delivery date is: ${dateInput.value}</p>
     <p class="delivery-time">Delivery time is: ${timeInput.value}</p>
@@ -219,23 +235,23 @@ function createModal() {
 }
 
 function citiesInputValidation() {
-  const err = document.querySelector(".cities__error");
-  const cities = Array.from(citiesList.children).map((el) => el.textContent);
+  const err = document.querySelector('.cities__error');
+  const cities = Array.from(citiesList.children).map(el => el.textContent);
 
   if (!citiesInput.value) {
     showInputErr(citiesInput, err);
   } else if (!cities.includes(citiesInput.value)) {
     showInputErr(citiesInput, err);
-    err.textContent = "This city is not on the list";
+    err.textContent = 'This city is not on the list';
   } else {
     undoError(citiesInput, err);
-    err.textContent = "";
+    err.textContent = '';
   }
 }
 
 function nameInputValidation() {
-  const nameInput = document.querySelector("#NameInput");
-  const err = document.querySelector(".name__error");
+  const nameInput = document.querySelector('#NameInput');
+  const err = document.querySelector('.name__error');
   if (!nameInput.value) {
     showInputErr(nameInput, err);
   } else {
@@ -244,8 +260,8 @@ function nameInputValidation() {
 }
 
 function surnameInputValidation() {
-  const surnameInput = document.querySelector("#SurnameInput");
-  const err = document.querySelector(".surname__error");
+  const surnameInput = document.querySelector('#SurnameInput');
+  const err = document.querySelector('.surname__error');
   if (!surnameInput.value) {
     showInputErr(surnameInput, err);
   } else {
@@ -254,7 +270,7 @@ function surnameInputValidation() {
 }
 
 function phoneInputValidation() {
-  const err = document.querySelector(".phone__error");
+  const err = document.querySelector('.phone__error');
   if (phoneMask.value.length !== 17) {
     showInputErr(phoneMask, err);
   } else {
@@ -263,21 +279,21 @@ function phoneInputValidation() {
 }
 
 function dateInputValidation() {
-  const err = document.querySelector(".date__error");
-  console.log(+getDeliveryDate().split("-")[2], +dateInput.value.split("-")[2]);
+  const err = document.querySelector('.date__error');
+  console.log(+getDeliveryDate().split('-')[2], +dateInput.value.split('-')[2]);
 
-  if (+getDeliveryDate().split("-")[2] > +dateInput.value.split("-")[2]) {
+  if (+getDeliveryDate().split('-')[2] > +dateInput.value.split('-')[2]) {
     showInputErr(dateInput, err);
     err.textContent = dateInput.validationMessage;
   } else {
     undoError(dateInput, err);
-    err.textContent = "";
+    err.textContent = '';
   }
 }
 
 function timeInputValidation() {
-  const err = document.querySelector(".time__error");
-  const inputHour = +timeInput.value.split(":")[0];
+  const err = document.querySelector('.time__error');
+  const inputHour = +timeInput.value.split(':')[0];
 
   if (inputHour < 9 || inputHour > 18) {
     showInputErr(timeInput, err);
@@ -287,18 +303,18 @@ function timeInputValidation() {
 }
 
 function showInputErr(input, err) {
-  input.style.outline = "2px solid red";
-  err.style.display = "inline-block";
+  input.style.outline = '2px solid red';
+  err.style.display = 'inline-block';
   input.focus();
 }
 
 function undoError(input, err) {
-  input.style.outline = "";
-  err.style.display = "";
+  input.style.outline = '';
+  err.style.display = '';
 }
 
 const maskOptions = {
-  mask: "+{38}(000)000-00-00",
+  mask: '+{38}(000)000-00-00',
 };
 const mask = IMask(phoneMask, maskOptions);
 
