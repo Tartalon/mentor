7d2c719b9a384e810614fe03ea46761b5b65be8c
Tartalon <tartalon23@gmail.com>
2022-04-13 20:56:56 +0300

    gulp

--- movie/gulp/config/path.js
+++ movie/gulp/config/path.js
@@ -6,12 +6,28 @@ const srcFolder = `./src`;
 
 export const path = {
   build: {
-    files: `${buildFolder}/files`,
+    js: `${buildFolder}/js/`,
+    images: `${buildFolder}/img/`,
+    css: `${buildFolder}/css/`,
+    html: `${buildFolder}/`,
+    fonts: `${buildFolder}/fonts/`,
+    files: `${buildFolder}/files/`,
   },
   src: {
+    js: `${srcFolder}/js/app.js`,
+    images: `${srcFolder}/img/**/*.{jpg, jpeg, png, gif, webp}`,
+    svg: `${srcFolder}/img/**/*.svg`,
+    scss: `${srcFolder}/scss/style.scss`,
+    html: `${srcFolder}/*.html`,
     files: `${srcFolder}/files/**/*.*`,
   },
-  watch: {},
+  watch: {
+    js: `${srcFolder}/**/*.js`,
+    scss: `${srcFolder}/**/*.scss`,
+    html: `${srcFolder}/**/*.html`,
+    images: `${srcFolder}/img/**/*.{jpg, jpeg, png, gif, webp, svg, ico}`,
+    files: `${srcFolder}/files/**/*.*`,
+  },
   clean: buildFolder,
   buildFolder: buildFolder,
   srcFolder: srcFolder,
